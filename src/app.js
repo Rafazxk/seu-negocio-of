099c6f143dc18977958d7d6bc01afba1514a319e
Rafazxk <rs2789115@gmail.com>
2026-04-17 23:15:29 -0300

    fix: resolve problema de caminho não renderizar no render

--- src/app.js
+++ src/app.js
@@ -4,28 +4,33 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import clienteRoutes from './routes/clienteRoutes.js';
 
+const app = express();
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const publicPath = path.resolve(__dirname, '..', 'public');
+
+
 app.use(cors());
+app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(publicPath));
+
 
 app.use('/clientes', clienteRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'usuario.html'));
-});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+// Fallback para rotas não encontradas (Redireciona para o index)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 export default app;
