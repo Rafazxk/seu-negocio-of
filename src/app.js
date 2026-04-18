786a59f2b285748936416c7249740fb0c4ffb308
Rafazxk <rs2789115@gmail.com>
2026-04-17 23:29:46 -0300

    fix: resolve problema de caminho para arquivo html 2

--- src/app.js
+++ src/app.js
@@ -2,34 +2,34 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import clienteRoutes from './routes/clienteRoutes.js';
+
+
+import clienteRoutes from './routes/clienteRoutes.js'; 
 
 const app = express();
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-const publicPath = path.resolve(__dirname, '..', './public');
+const publicPath = path.join(__dirname, '..', 'public');
 
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use(express.static(publicPath));
 
 
-app.use('/clientes', clienteRoutes);
-
+app.use('/api/clientes', clienteRoutes); 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-// Fallback para rotas não encontradas (Redireciona para o index)
-app.get('*', (req, res) => {
+app.get('*', (req, res, next) => {
+  if (req.url.startsWith('/api')) return next();
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
