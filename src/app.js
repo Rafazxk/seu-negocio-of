20793d95aa70a0aa824de0f781e63bbaa3222930
Rafazxk <rs2789115@gmail.com>
2026-04-17 23:18:01 -0300

    fix: resolve problema de caminho para arquivo html

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const publicPath = path.resolve(__dirname, '..', 'public');
+const publicPath = path.resolve(__dirname, '..', './public');
 
 
 app.use(cors());
